fddb47c460add6ac2cc682ba838bcd70d1766a23
Santicoo <santyy6406@gmail.com>
2024-09-23 13:34:02 -0500

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,10 @@
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+
+
+n = float(input("Ingrese el número del cual desea saber el factorial: "))
+print("Resultado:", factorial(n))
